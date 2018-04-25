9ada4813cbb6f22cf496c4a6baf56e5a3d4d2ac7
Kertic <coolbman200@gmail.com>
2018-04-25 15:35:17 -0400

    Adding Exploring Data Types

---
+++ 3.ExploringDataTypes/3.ExploringDataTypes/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/**
+ * Goals:
+ * Create and print one of each data set (no copy and paste allowed)
+ * 
+ * From this lesson on, any command that has previously been allowed will be allowed in all future lessons.
+ * Any commands that can't be used will be explicilty written
+ * 
+ */
+namespace _3.ExploringDataTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //int
+            //Integer
+            //Used for whole number based math
+            int friendsThatIHave = 0;
+
+            //float
+            //Floating point
+            //Used for numbers that may need a decimal place, but don't need to be incredibly precise (general use decimal)
+            float pi = 3.14f;
+
+            //double
+            //Double 
+            //Used for numbers where precision is critical and decimals will be involved
+            double surgicalLocation = 0.00000022;
+
+            //bool
+            //Boolean value
+            //A value that either is "True" or "False"
+            bool doesMyMomLoveMe = true;
+
+            //char
+            //Character
+            //A single alpha-numeric character (a-z, 0-9, weird symbols)
+            char theLetterA = 'a';
+
+            //string
+            //An array or "string" of characters
+            //A set of characters, used to represent entire words or phrases
+            string andHisNameIs = "JOHN CENA DUN DUN DUH DUUHHHHHHH";
+
+
+
+            //Create your variables here
+
+            //Print them here
+        }
+    }
+}
