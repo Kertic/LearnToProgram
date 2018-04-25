f6c2d6a19fa8ca72d754884dc2cfbbd9eed1a7a0
Kertic <coolbman200@gmail.com>
2018-04-25 12:06:35 -0400

    Added HelloWorld

---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /**
+             *Hello World:
+             * Goals: Print "Hello World!" to the console
+             * 
+             * Usable commands:
+             * Console.Write();
+             * Console.WriteLine();
+             * Console.ReadLine(); 
+             * Console.ReadKey();
+             */
+
+            //Start Code Here
+        }
+    }
+}
