1fbcfb8f9c5f76a11b818bc5b2e200b181aa136e
Kertic <coolbman200@gmail.com>
2018-04-25 12:24:20 -0400

    Added DoSomeMath

---
+++ DoSomeMath/DoSomeMath/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+/**
+ * Goals: Perform the following math operations and store their answers in a variable, and print out the answer using the variable
+ * 
+ * Math: 5+5
+ * Varialbe Name: Ans1
+ * Math: 5-10
+ * Varialbe Name: Ans2
+ * Math: 3*2
+ * Varialbe Name: Ans3
+ * Math: 4/4
+ * Varialbe Name: Ans4
+ * 
+ * 
+ * 
+ * Usable commands:
+ * +
+ * -
+ * /
+ * *
+ * =
+ */
+
+
+namespace DoSomeMath
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            //Example
+            int ExampleAnswer = 42 - 1;
+
+
+            //Write your code here
+
+
+            //Remove the comments below to print out your answers
+            //Console.WriteLine("5+5 = " + Ans1);
+            //Console.WriteLine("5-10 = " + Ans2);
+            //Console.WriteLine("3*2 = " + Ans3);
+            //Console.WriteLine("4/4 = " + Ans4);
+        }
+    }
+}
